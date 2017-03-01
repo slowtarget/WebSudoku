38ffac76190e27a6d60915b1c8ab76b53e96f3be
Jason Farmer <jason.farmer@travisperkins.co.uk>
2017-03-01 17:02:17 +0000

    Candidate now translates values into bitmasks via a string mask...

--- Candidates.js
+++ Candidates.js
@@ -1,5 +1,17 @@
-function Candidates(n) {
-  this._mask = n;
+function Candidates(mask) {
+  this._mask = mask;
+  if ( BoardSize == 9 ) {
+    this._translate = "123456789";
+  }
+  if ( BoardSize == 16 ) {
+    this._translate = "0123456789ABCDEF";
+  }
+  if ( BoardSize == 4 ) {
+    this._translate = "1234";
+  }
+  if ( BoardSize == 25 ) {
+    this._translate = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+  }
 }
 
 Candidates.prototype.remove = function (mask) {
@@ -7,28 +19,37 @@ Candidates.prototype.remove = function (mask) {
   return this._mask;
 };
 
+Candidates.prototype.removeValue = function(n) {
+  index = this._translate.indexOf(n)
+  if (index < 0) return false;
+  this._mask &= ~(1 << index);
+};
+
 Candidates.prototype.getSingle = function () {
   // Count number of on bits from 1..9
   var single = 0;
   var count = 0;
-  for (var i = 1; i <= BoardSize; i++)
+  for (var i = 0; i < BoardSize; i++)
     if ((this._mask & (1 << i)) != 0) {
       count++;
       single = i;
     }
-  return count == 1 ? single : null;
+  return count == 1 ? this._translate[single] : null;
 };
+
 
 // Used when the answer is known at the Cell, level this sets the only allowed value to be that answer
 Candidates.prototype.setSingle = function (n) {
-  this._mask = 1 << n;
+  index = this._translate.indexOf(n)
+  if (index < 0) return false;
+  this._mask = 1 << index;
 };
 
 Candidates.prototype.count = function () {
   // Count number of on bits from 1..9
   var count = 0;
-  for (var i = 1; i <= BoardSize; i++)
-    if ((this._mask & (1 << i)) != 0)
+  for (var i = 0; i < BoardSize; i++)
+    if ((this._mask & (1 << i )) != 0)
       count++;
   return count;
 };
@@ -37,9 +58,6 @@ Candidates.prototype.isAllowed = function (n) {
   return n >= 1 && n <= BoardSize && ((this._mask & (1 << n)) != 0);
 };
 
-Candidates.prototype.removeValues = function (bm) {
-  this._mask &= ~bm._mask;
-};
 
 Candidates.prototype.CandidatesArray = function () {
   var ret = new Array();
