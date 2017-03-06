0e55cae8e5ade412c0906ad7951e285a5e3d2e0b
Jason Farmer <jason.farmer@travisperkins.co.uk>
2017-03-06 17:00:41 +0000

    enhance hidden single test

--- tests/hiddenSingle.js
+++ tests/hiddenSingle.js
@@ -7,13 +7,11 @@ var cell = new Cell(1);  cell.removeCandidate("1"); cell.removeCandidate("3"); g
 var cell = new Cell(2);  cell.removeCandidate("1"); cell.removeCandidate("2"); group.add(cell); cell.addGroup(group); cells.push(cell);
 var cell = new Cell(3);  cell.removeCandidate("1"); cell.removeCandidate("2"); group.add(cell); cell.addGroup(group); cells.push(cell);
 
-var exp =[];
-exp.push(true);
-exp.push(true);
-exp.push(false);
-exp.push(false);
+var exp = [true,true,false,false];
+var ans = [1,2,null,null];
 
 for (var i=0;i<cells.length;i++){
   var act = cells[i].hiddenSingles();
   writeTest("1.1."+i,"found?",exp[i],act,(act === exp[i]));
+  writeTest("1.2."+i,"answer?",ans[i],cells[i].getAnswer(),(act === exp[i]));
 }
